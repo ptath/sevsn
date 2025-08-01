e989840522fe1f2d4c66ff8f4f827d39bb4ef15a
ptath <ptath@ptath.ru>
2025-08-02 01:07:05 +0400

    Update 010-neighbour.sh
    Добавлены название провайдера и сервер cloudflare

--- 010-neighbour.sh
+++ 010-neighbour.sh
@@ -8,6 +8,12 @@
 # kadastr_no="919"
 kadastr_no="919"
 
+# наименование провайдера
+# нужно указать провайдера из списка (с появлением новых провайдеров список будет расширяться):
+#   Дом.ру
+#   iKorp
+provider_name="Дом.ру"
+
 # данные для подключения к серверу 
 # mqtt_username и mqtt_password нужно взять у председателя
 mqtt_username=000000
@@ -23,16 +29,20 @@ mqtt_server="mqtt.sevsn.ru"
 mqtt_port="1883"
 
 # MQTT топик с результатом проверки доступности Яндекса
-mqtt_topic="users/"$kadastr_no"/internet/yandex"
+mqtt_topic="users/"$kadastr_no"/internet"
 
-# команда отправляет четыре PING запроса к серверу Яндекса
+# команда отправляет три PING запроса к серверам Яндекса и Cloudflare
 # и ждет ответа одну секунду
 # если нет ответа в срок хотя бы на один запрос, завершается ошибкой
 # успешное завершение выдает 10 цифр — время в формате UNIX 
 # (количество секунд, прошедших с полуночи (00:00:00 UTC) 1 января 1970 года)
 # неуспешное завершение выдает 0 — 1 января 1970 года
-yandex_check_time=$(ping -c 4 -W 1 yandex.ru  > /dev/null; if [ $? -eq 0 ]; then echo $(date +%s); else echo "0"; fi)
+yandex_check_time=$(ping -c 3 -W 1 yandex.ru  > /dev/null; if [ $? -eq 0 ]; then echo $(date +%s); else echo "0"; fi)
 
-# команда отправляет результат предыдущей команды на
-# MQTT сервер ТСН в топик users/НОМЕР_УЧАСТКА/internet/yandex
-mosquitto_pub -d -t $mqtt_topic -m $yandex_check_time -h $mqtt_server -p $mqtt_port -u $mqtt_username -P $mqtt_password
+cloudflare_check_time=$(ping -c 3 -W 1 1.1.1.1  > /dev/null; if [ $? -eq 0 ]; then echo $(date +%s); else echo "0"; fi)
+
+# команда отправляет результаты предыдущих команд на
+# MQTT сервер ТСН в топик users/НОМЕР_УЧАСТКА/internet
+mosquitto_pub -d -t $mqtt_topic -m $provider_name -h $mqtt_server -p $mqtt_port -u $mqtt_username -P $mqtt_password
+mosquitto_pub -d -t $mqtt_topic"/yandex" -m $yandex_check_time -h $mqtt_server -p $mqtt_port -u $mqtt_username -P $mqtt_password
+mosquitto_pub -d -t $mqtt_topic"/cloudflare" -m $cloudflare_check_time -h $mqtt_server -p $mqtt_port -u $mqtt_username -P $mqtt_password
