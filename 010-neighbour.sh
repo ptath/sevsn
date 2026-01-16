230f50f38b4cb9514cf41850f887eb948f5753d0
ptath <ptath@ptath.ru>
2026-01-16 13:58:52 +0400

    Update 010-neighbour.sh
    Добавлено автоматическое определение провайдера
    
    Переработан вывод в формат JSON

--- 010-neighbour.sh
+++ 010-neighbour.sh
@@ -8,41 +8,80 @@
 # kadastr_no="919"
 kadastr_no="919"
 
-# наименование провайдера
-# нужно указать провайдера из списка (с появлением новых провайдеров список будет расширяться):
-#   Дом.ру
-#   iKorp
-provider_name="Дом.ру"
-
 # данные для подключения к серверу 
 # mqtt_username и mqtt_password нужно взять у председателя
 mqtt_username=000000
-mqtt_password=PASSWORD
+mqtt_password=password
 
 ### КОНЕЦ РАЗДЕЛА ДЛЯ РЕДАКТИРОВАНИЯ
 ### ДАЛЬШЕ НИЧЕГО ТРОГАТЬ НЕ НУЖНО
 
-# адрес сервера
+# Настройки сервера
 mqtt_server="mqtt.sevsn.ru"
-
-# порт
 mqtt_port="1883"
+mqtt_topic="users/$kadastr_no/internet_state"
 
-# MQTT топик с результатом проверки доступности Яндекса
-mqtt_topic="users/"$kadastr_no"/internet"
+# Функция: определить провайдера по публичному IP
+get_provider_by_ip() {
+    # Получаем публичный IP (через внешний сервис)
+    public_ip=$(curl -s https://api.ipify.org)
+    if [ -z "$public_ip" ]; then
+        echo "Не удалось определить IP адрес"
+        return
+    fi
 
-# команда отправляет три PING запроса к серверам Яндекса и Cloudflare
-# и ждет ответа одну секунду
-# если нет ответа в срок хотя бы на один запрос, завершается ошибкой
-# успешное завершение выдает 10 цифр — время в формате UNIX 
-# (количество секунд, прошедших с полуночи (00:00:00 UTC) 1 января 1970 года)
-# неуспешное завершение выдает 0 — 1 января 1970 года
-yandex_check_time=$(ping -c 3 -W 1 yandex.ru  > /dev/null; if [ $? -eq 0 ]; then echo $(date +%s); else echo "0"; fi)
+    # Делаем whois-запрос и ищем организацию
+    # (ВНИМАНИЕ!!! Требуется установить whois - opkg install whois)
+    whois_data=$(whois "$public_ip" 2>/dev/null)
+    
+    # Проверяем ключевые слова
+    case "$whois_data" in
+        *Dom.ru*|*DOM.RU*|*ERTH*|*"Дом.ру"*)
+            echo "Дом.ру"
+            ;;
+        *SCTS*|*IKORP*)
+            echo "iKorp"        
+            ;;
+        *TELE2*|*tele2*)
+            echo "TELE2"        
+            ;;                    
+        *Rostelecom*|*Ростелеком*)
+            echo "Ростелеком"
+            ;;
+        *Megafon*|*Мегафон*)
+            echo "Мегафон"
+            ;;
+        *MT_Russia*|*MTS*|*МТС*)
+            echo "МТС"
+            ;;
+        *)
+            # Если не нашли
+            echo "Неизвестный провайдер"
+            ;;
+    esac
+}
 
-cloudflare_check_time=$(ping -c 3 -W 1 1.1.1.1  > /dev/null; if [ $? -eq 0 ]; then echo $(date +%s); else echo "0"; fi)
+# Получаем название провайдера
+provider_name=$(get_provider_by_ip)
 
-# команда отправляет результаты предыдущих команд на
-# MQTT сервер ТСН в топик users/НОМЕР_УЧАСТКА/internet
-mosquitto_pub -d -t $mqtt_topic -m $provider_name -h $mqtt_server -p $mqtt_port -u $mqtt_username -P $mqtt_password
-mosquitto_pub -d -t $mqtt_topic"/yandex" -m $yandex_check_time -h $mqtt_server -p $mqtt_port -u $mqtt_username -P $mqtt_password
-mosquitto_pub -d -t $mqtt_topic"/cloudflare" -m $cloudflare_check_time -h $mqtt_server -p $mqtt_port -u $mqtt_username -P $mqtt_password
+# Проверяем доступность Яндекса
+yandex_check_time=$(ping -c 3 -W 1 yandex.ru > /dev/null 2>&1; \
+    if [ $? -eq 0 ]; then date +%s; else echo "0"; fi)
+
+# Проверяем доступность Cloudflare
+cloudflare_check_time=$(ping -c 3 -W 1 1.1.1.1 > /dev/null 2>&1; \
+    if [ $? -eq 0 ]; then date +%s; else echo "0"; fi)
+
+# Формируем JSON
+json_payload="{\"provider\": \"$provider_name\", 
+		\"yandex_check_time\": $yandex_check_time, 
+		\"cloudflare_check_time\": $cloudflare_check_time}"
+
+# Публикуем JSON в MQTT
+mosquitto_pub -d \
+    -t "$mqtt_topic" \
+    -m "$json_payload" \
+    -h "$mqtt_server" \
+    -p "$mqtt_port" \
+    -u "$mqtt_username" \
+    -P "$mqtt_password"
